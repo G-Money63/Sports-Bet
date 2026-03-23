6527e911815ee6ceb299932dce578ed7f6e5ac78
G-Money63 <garyjrubens@gmail.com>
2026-03-23 17:07:47 +0000

    v1.3 update

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path    = require('path');
+const app = express();
+
+app.use(express.json({ limit: '2mb' }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
+
+app.get('/',           (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/index.html', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
